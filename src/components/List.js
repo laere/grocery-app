11b9be470e583fdde29a8d2b9ed1509823bffd71
Zack Bostian <bostianz@gmail.com>
2016-04-18 15:28:48 -0400

    got basic state shape done, and a couple actions

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,36 @@
 import React, { Component, PropTypes } from 'react';
 import ListItem from './ListItem';
+import { addItem, userInput } from '../reducers/GroceryReducer';
 
-export default class List extends Component {
+class List extends React.Component {
+  static propTypes = {
+    addGroceryItem: PropTypes.func.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleAddItem(e) {
+    const { addGroceryItem, text } = this.props;
+    addGroceryItem(text);
+  }
+
+  handleDeleteItem(e) {
+    
+  }
+
+  handleOnChange(e) {
+    const { getInput } = this.props;
+    getInput(e.target.value);
+  }
+
   render() {
     return (
       <div>
         <div>
-          <input type="text" />
+          <input type="text" onChange={this.handleOnChange}/>
           <button>Add</button>
         </div>
         <div>
@@ -18,3 +42,19 @@ export default class List extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    items: state.items,
+    text: state.text
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addGroceryItem: (text) => dispatch(addItem(text)),
+    getInput: (input) => dispatch(userInput(input))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(List);
--- src/reducers/GroceryReducer.js
+++ src/reducers/GroceryReducer.js
@@ -1,24 +1,71 @@
+
 const actions = {
   ADD_ITEM: 'ADD_ITEM',
   DELETE_ITEM: 'DELETE_ITEM',
-  COMPLETE_ITEM: 'COMPLETE_ITEM'
+  COMPLETE_ITEM: 'COMPLETE_ITEM',
+  GET_INPUT: 'GET_INPUT'
 };
+
+export const addItem = (text) => {
+  return (dispatch) => {
+    dispatch({
+      type: actions.ADD_ITEM,
+      text
+    })
+  }
+}
+
+export const userInput = (input) => {
+  return {
+    type: actions.GET_INPUT,
+    input
+  }
+}
 
 const initialState = {
   items: [],
-  input: '',
-  isComplete: false
+  // itemIds: [],
+  // itemsById: {},
+  groceryItem: {
+    id: 0,
+    text: null,
+    receivedAt: null,
+    isComplete: false
+  }
 }
 
-export default function(state = initialState, action) {
+function items(state = initialState, action) {
   switch(action.type) {
     case actions.ADD_ITEM:
-      return state;
+      return {
+        ...state,
+        items: state.items.concat({
+          groceryItem: {
+            ...state,
+            itemId: state.id++,
+            text: action.text,
+            receivedAt: Date.now(),
+            isComplete: false
+          }
+        })
+      }
     case actions.DELETE_ITEM:
       return state;
+    default:
+      return state;
+  }
+}
+
+function completeItem(state = initialState.groceryItem.isComplete, action) {
+  switch(action.type) {
     case actions.COMPLETE_ITEM:
       return state;
-    default:
+  }
+}
+
+function changeInput(state = initialState.groceryItem.text, action) {
+  switch(action.type) {
+    case actions.GET_INPUT:
       return state;
   }
 }
