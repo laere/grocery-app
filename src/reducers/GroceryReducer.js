dff0bc4a806707ff99b1c12525f0ab20961e7383
Zack Bostian <bostianz@gmail.com>
2016-04-24 14:56:29 -0400

    added bluebird

--- src/reducers/GroceryReducer.js
+++ src/reducers/GroceryReducer.js
@@ -5,19 +5,17 @@ import * as actions from '../actions/actions';
 let nextId = 0;
 
 export function items(state = [], action) {
-  // const rootRef = 'https://zacksgroceryapp.firebaseio.com/';
-  // const listRef = new Firbase(rootRef + 'grocerylist');
   const index = _.findIndex(state, x => x.id === action.id),
         url = 'https://zacksgroceryapp.firebaseio.com/',
         groceryListRef = new Firebase(url + 'grocerylist');
   switch(action.type) {
     case actions.RENDER_ITEMS:
       return state.concat(action.items);
-    // case actions.ADD_ITEM:
-    //   return groceryListRef.on('child_added', snap => {
-    //     state.push(snap.value())
-    //   });
-    //   return state;
+    case actions.ADD_ITEM:
+      return groceryListRef.on('child_added', snap => {
+        state.push(snap.value())
+      });
+      return state;
     case actions.DELETE_ITEM:
       return dotProp.delete(state, `${index}`);
     case actions.COMPLETE_ITEM:
